23f724f280401b5f7655be8a1931b8bcd5b3bf43
Rabab <rababahmed6@gmail.com>
2021-03-26 15:11:56 +0600

    add barebones function to process api

---
+++ src/components/flights.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import "../styles/main.css";
+
+const FlightView = (props) => {
+  return (
+    <div>
+      <div className="flight-label">Flight Data</div>
+      <div class="flight-container">
+        <div className="flight-card">
+          <div className="f-1">{props.data.Carriers[0].Name}</div>
+          <div className="f-2">$ {props.data.Quotes[0].MinPrice}</div>
+          <div className="f-3">
+            Departs at {props.data.Quotes[0].OutboundLeg.DepartureDate}
+          </div>
+          <div className="f-4">Depart from {props.data.Places[0].Name}</div>
+          <div className="f-5">Arrive at {props.data.Places[1].Name}</div>
+        </div>
+        <div className="flight-card">
+          <div className="f-1">{props.data.Carriers[1].Name}</div>
+          <div className="f-2">$ {props.data.Quotes[1].MinPrice}</div>
+          <div className="f-3">
+            Departs at {props.data.Quotes[1].OutboundLeg.DepartureDate}
+          </div>
+          <div className="f-4">Depart from {props.data.Places[0].Name}</div>
+          <div className="f-5">Arrive at {props.data.Places[1].Name}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FlightView;
---
+++ src/components/main.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from "react";
+import "../styles/main.css";
+import FlightView from "./flights";
+import axios from "axios";
+
+const Main = () => {
+  const [origin, setOrigin] = useState("");
+  const [destination, setDestination] = useState("");
+  const [flightData, setFlightData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log({ origin, destination });
+
+    const options = {
+      method: "GET",
+      url:
+        "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/" +
+        origin +
+        "-sky/" +
+        destination +
+        "-sky/anytime",
+      headers: {
+        "x-rapidapi-key": "31799ff833mshdc413de7a349e59p15c832jsnfd2a01775473",
+        "x-rapidapi-host":
+          "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        setFlightData(response.data);
+        setLoading(false);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  return (
+    <div class="parent-container">
+      <div className="form-container">
+        <form id="form" onSubmit={submitHandler}>
+          <label htmlFor="origin">Flying from</label>
+          <input
+            onChange={(e) => setOrigin(e.target.value)}
+            type="text"
+            name="origin"
+            placeholder="DAC"
+            id=""
+            value={origin}
+          />
+          <label htmlFor="destination">Flying to</label>
+          <input
+            onChange={(e) => setDestination(e.target.value)}
+            type="text"
+            name="destination"
+            id=""
+            placeholder="Eg. LHR"
+            value={destination}
+          />
+          <button type="submit">Search Flights</button>
+        </form>
+      </div>
+      {loading ? (
+        <div>Search for any flight</div>
+      ) : (
+        <FlightView data={flightData} />
+      )}
+    </div>
+  );
+};
+
+export default Main;
